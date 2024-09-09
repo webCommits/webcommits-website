68ebb3e023a3ed289c3bf55a86f04858167a8cd9
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2024-09-09 10:35:06 +0200

    Added down arrow and margins

--- src/static/script.js
+++ src/static/script.js
@@ -41,6 +41,7 @@ function animateHeader() {
 document.addEventListener('DOMContentLoaded', function() {
     showTopButton();
     animateHeader();
+    smoothScrollWithOffset('#scroll-arrow');
 })
 
 const observer = new IntersectionObserver((entries => {
@@ -101,4 +102,16 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+function smoothScrollWithOffset(selector, offset = 200) {
+    const link = document.querySelector(selector);
+    link.onclick = function(e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute('href'));
+      window.scrollTo({
+        top: target.offsetTop - offset,
+        behavior: 'smooth'
+      });
+    };
+  }
 
