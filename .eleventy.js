6f483bb5c19f26847ee828f5a71174c6309f9b58
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2024-12-20 22:50:36 +0100

    Added passthrough for sitemap/robots

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy("src/static/script.js")
     eleventyConfig.addPassthroughCopy("src/static/images")
     eleventyConfig.addPassthroughCopy("src/static/svg")
+    eleventyConfig.addPassthroughCopy("src/sitemap.xml")
+    eleventyConfig.addPassthroughCopy("src/robots.txt")
     return {
         dir: {
           input: "src",
