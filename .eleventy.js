a4e896fd7822138c8da3540a1ee8434f646b358b
LStoneyy <123068985+LStoneyy@users.noreply.github.com>
2025-04-15 17:24:14 +0200

    Added changes from main and paths

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
-module.exports = function(eleventyConfig){
+module.exports = function(eleventyConfig) {
+  
+  // 💡 baseurl global verfügbar machen
+  eleventyConfig.addGlobalData("global", require("./_data/global.json"))
 
-    eleventyConfig.addPassthroughCopy("src/static/style.css")
-    eleventyConfig.addPassthroughCopy("src/static/script.js")
-    eleventyConfig.addPassthroughCopy("src/static/images")
-    eleventyConfig.addPassthroughCopy("src/static/svg")
-    eleventyConfig.addPassthroughCopy("src/sitemap.xml")
-    eleventyConfig.addPassthroughCopy("src/robots.txt")
-    return {
-        dir: {
-          input: "src",
-          output: "docs"
-        }
-      };
+  eleventyConfig.addPassthroughCopy("src/static/style.css")
+  eleventyConfig.addPassthroughCopy("src/static/script.js")
+  eleventyConfig.addPassthroughCopy("src/static/images")
+  eleventyConfig.addPassthroughCopy("src/static/svg")
+  eleventyConfig.addPassthroughCopy("src/sitemap.xml")
+  eleventyConfig.addPassthroughCopy("src/robots.txt")
+  eleventyConfig.addPassthroughCopy("_data")
+
+  return {
+    dir: {
+      input: "src",
+      output: "docs"
+    }
+  };
 }
